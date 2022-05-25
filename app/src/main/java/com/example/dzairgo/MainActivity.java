42c9843da4065dd00273dffeff25266f1944722b
yacinetouahria <medyacinetouahria@gmail.com>
2022-05-25 20:27:57 +0100

    toggle password view

--- app/src/main/java/com/example/dzairgo/MainActivity.java
+++ app/src/main/java/com/example/dzairgo/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView eye ;
     EditText psw;
+    int psw_visible = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
         eye= (ImageView) findViewById(R.id.eye_icon);
         psw = (EditText) findViewById(R.id.psw_edittext);
+
         eye.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(psw.isShown()){
-                    psw.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                if(psw_visible == -1){
+                    psw.setTransformationMethod(null);
+                    psw.setInputType(InputType.TYPE_CLASS_TEXT);
+                    psw_visible = 1;
                 }else{
-                    psw.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    psw.setTransformationMethod(new PasswordTransformationMethod());
+                    psw.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    psw_visible = -1;
                 }
             }
         });
