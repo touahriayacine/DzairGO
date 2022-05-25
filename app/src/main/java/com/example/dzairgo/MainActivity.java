c76402d658e243009b2e1c6c8ada7f0abef28d0a
yacinetouahria <medyacinetouahria@gmail.com>
2022-05-25 13:48:47 +0100

    login page changes

--- app/src/main/java/com/example/dzairgo/MainActivity.java
+++ app/src/main/java/com/example/dzairgo/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.dzairgo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView eye ;
+    EditText psw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eye= (ImageView) findViewById(R.id.eye_icon);
+        psw = (EditText) findViewById(R.id.psw_edittext);
+        eye.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(psw.isShown()){
+                    psw.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                }else{
+                    psw.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                }
+            }
+        });
     }
 }
