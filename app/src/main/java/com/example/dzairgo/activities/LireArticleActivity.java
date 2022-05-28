49d4e8d4457f114753fb5c303c60b76827621638
yacinetouahria <medyacinetouahria@gmail.com>
2022-05-29 00:33:40 +0100

    add some changes

--- app/src/main/java/com/example/dzairgo/activities/LireArticleActivity.java
+++ app/src/main/java/com/example/dzairgo/activities/LireArticleActivity.java
@@ -132,8 +132,9 @@ public class LireArticleActivity extends AppCompatActivity {
     ArrayList<Article> generArticles(){
         ArrayList<Article> articles= new ArrayList<>();
         Map<ElementType, Object> structure1 = new HashMap<>();
-        structure1.put(ElementType.TEXT , "Le palais des raïs, ou bastion 23, est un monument historique et architectural classé. Il est aussi un centre polyvalent d'art et de culture. Ouvert au publique depuis novembre 1994. Il est constitué de trois palais, cinq maisons, un passage ouvert, une coure centrale, un chemin de ronde et une batterie.\n" +
-                "Ce palais demeure le dernier et unique témoin du prolongement de la Casbah jusqu'à la mer. Il offre aujourd'hui, aux visiteurs, la possibilité de se promener dans un environnement historique et culturel exceptionnel\n");
+
+        structure1.put(ElementType.TEXT , "Le palais des raïs, ou bastion 23, est un monument historique et architectural classé. Il est aussi un centre polyvalent d'art et de culture. Ouvert au publique depuis novembre 1994. Il est constitué de trois palais, cinq maisons, un passage ouvert, une coure centrale, un chemin de ronde et une batterie. Ce palais demeure le dernier et unique témoin du prolongement de la Casbah jusqu'à la mer. Il offre aujourd'hui, aux visiteurs, la possibilité de se promener dans un environnement historique et culturel exceptionnel");
+
         structure1.put(ElementType.IMAGE , this.getDrawable(R.drawable.place_1));
         ArrayList<Commentaire> commentaires1 = new ArrayList<>();
         commentaires1.add(this.commentaires.get(0));
@@ -142,8 +143,9 @@ public class LireArticleActivity extends AppCompatActivity {
         articles.add(new Article("Palais des raïs (Bastion 23)" , this.getDrawable(R.drawable.place_1) , "27 Mai 2022" , "18:57" , 2 ,contenu1 , "Alger" ));
 
         Map<ElementType, Object> structure2 = new HashMap<>();
-        structure2.put(ElementType.TEXT , "Un parc paysager installé sur une pente abrupte, en plein centre d'Alger, dans un quartier résiduel très calme. Le parc Beyrout est toujours en très bon état, avec ses mêmes éclats d'antan. Le lieu abrite une variété d'Arbres ombragés des allées spacieux et des espaces verts parfaitement entretenus et gardés.\n" +
-                "Le lieu dispose également d'un aire de jeux pour enfants, et il offre, même de loin, une vue sur la majestueuse baie d'Alger \n");
+
+        structure2.put(ElementType.TEXT , "Un parc paysager installé sur une pente abrupte, en plein centre d'Alger, dans un quartier résiduel très calme. Le parc Beyrout est toujours en très bon état, avec ses mêmes éclats d'antan. Le lieu abrite une variété d'Arbres ombragés des allées spacieux et des espaces verts parfaitement entretenus et gardés. Le lieu dispose également d'un aire de jeux pour enfants, et il offre, même de loin, une vue sur la majestueuse baie d'Alger \n");
+
         structure2.put(ElementType.IMAGE , this.getDrawable(R.drawable.place_2));
         ArrayList<Commentaire> commentaires2 = new ArrayList<>();
         commentaires2.add(this.commentaires.get(2));
@@ -152,8 +154,9 @@ public class LireArticleActivity extends AppCompatActivity {
         articles.add(new Article("Parc de Beyrout (Mont riant)" , this.getDrawable(R.drawable.place_2) , "27 Mai 2022" , "19:08" , 2 ,contenu2 , "Alger" ));
 
         Map<ElementType, Object> structure3 = new HashMap<>();
-        structure3.put(ElementType.TEXT , "«Ce musée qui ambitionne d'avoir une vocation historique, ethnologique et d'interprétation scientifique, se situe au pied de la Casbah d'Alger dans les imposantes \"Voûtes Khireddine\", construites en 1814 par Hadj Ali Pacha et qui ont servi d'atelier de réparation de la flotte sous la régence Ottoman avant que les forces coloniales françaises n'y installent de grands fours pour fournir du pain à leurs soldats.\n" +
-                "Remontant jusqu'aux périodes, préhistorique, antique, médiévale et ottomane, le \"Musée de la marine\", œuvre à \"restituer la vie de l'Homme depuis ses premiers contacts avec la mer\", en focalisant sur la période ottomane de \"La Régence d'Alger\" qui a précédé la colonisation française de l'Algérie» Explique Mme Amel Mokrani Boukari, directrice du musée");
+
+        structure3.put(ElementType.TEXT , "«Ce musée qui ambitionne d'avoir une vocation historique, ethnologique et d'interprétation scientifique, se situe au pied de la Casbah d'Alger dans les imposantes Voûtes Khireddine, construites en 1814 par Hadj Ali Pacha et qui ont servi d'atelier de réparation de la flotte sous la régence Ottoman avant que les forces coloniales françaises n'y installent de grands fours pour fournir du pain à leurs soldats. Remontant jusqu'aux périodes, préhistorique, antique, médiévale et ottomane, le Musée de la marine, œuvre à restituer la vie de l'Homme depuis ses premiers contacts avec la mer, en focalisant sur la période ottomane de La Régence d'Alger qui a précédé la colonisation française de l'Algérie» Explique Mme Amel Mokrani Boukari, directrice du musée");
+
         structure3.put(ElementType.IMAGE , this.getDrawable(R.drawable.place_3));
         ArrayList<Commentaire> commentaires3 = new ArrayList<>();
         commentaires3.add(this.commentaires.get(0));
@@ -161,20 +164,18 @@ public class LireArticleActivity extends AppCompatActivity {
         articles.add(new Article("Musée nationale publique maritime " , this.getDrawable(R.drawable.place_3) , "27 Mai 2022" , "19:20" , 1 ,contenu3 , "Alger" ));
 
         Map<ElementType, Object> structure4 = new HashMap<>();
-        structure4.put(ElementType.TEXT , "Ce port est un endroit de grand repère dans l'histoire de l'Algérie, marquant le débarquement de l'armée française, un 5 juillet 1830\n" +
-                "Il compte quatre hôtels classés, ainsi que plusieurs restaurants implémentés pour se restaurer si besoin.\n" +
-                "Le parking est disponible pour 200da.\n" +
-                "On y propose également des petits tours de 45 mins en poney ou en bateau, pour s'offrir un moment agréable et paisible et profiter de la vue magnifique de ce port ");
+
+        structure4.put(ElementType.TEXT , "Ce port est un endroit de grand repère dans l'histoire de l'Algérie, marquant le débarquement de l'armée française, un 5 juillet 1830. Il compte quatre hôtels classés, ainsi que plusieurs restaurants implémentés pour se restaurer si besoin. Le parking est disponible pour 200da. On y propose également des petits tours de 45 mins en poney ou en bateau, pour s'offrir un moment agréable et paisible et profiter de la vue magnifique de ce port ");
+
         structure4.put(ElementType.IMAGE , this.getDrawable(R.drawable.place_4));
         ArrayList<Commentaire> commentaires4 = new ArrayList<>();
         Contenu contenu4 = new Contenu(structure4, commentaires4);
         articles.add(new Article("Port de Sidi Fredj" , this.getDrawable(R.drawable.place_4) , "27 Mai 2022" , "21:50" , 0 ,contenu4 , "Alger" ));
 
         Map<ElementType, Object> structure5 = new HashMap<>();
-        structure5.put(ElementType.TEXT , "L'ancien parc de Galland a été inauguré en 1915 par Charles de Galland, maire d'Alger de 1910 à 1919 et proviseur du lycée de Ben Aknoun, qui fit don à la ville de ce magnifique terrain.\n" +
-                "Le parc vient tout juste d'être superbement restauré et il est donc encore plus beau depuis 2018. \n" +
-                "Ses jardins en terrasses entrecoupés d'escaliers, ses volières et son plan d'eau en faisant alors l'un des lieux de promenades les plus appréciés des Algérois et l'un des plus élégants parc de la ville.\n" +
-                "Un parc très propre et recommendé pour tout amoureux des lieux de paix et de prospérité ");
+
+        structure5.put(ElementType.TEXT , "L'ancien parc de Galland a été inauguré en 1915 par Charles de Galland, maire d'Alger de 1910 à 1919 et proviseur du lycée de Ben Aknoun, qui fit don à la ville de ce magnifique terrain. Le parc vient tout juste d'être superbement restauré et il est donc encore plus beau depuis 2018. Ses jardins en terrasses entrecoupés d'escaliers, ses volières et son plan d'eau en faisant alors l'un des lieux de promenades les plus appréciés des Algérois et l'un des plus élégants parc de la ville. Un parc très propre et recommendé pour tout amoureux des lieux de paix et de prospérité ");
+
         structure5.put(ElementType.IMAGE , this.getDrawable(R.drawable.place_5));
         ArrayList<Commentaire> commentaires5 = new ArrayList<>();
         commentaires5.add(this.commentaires.get(0));
--- app/src/main/java/com/example/dzairgo/fragments/ActualityFragment.java
+++ app/src/main/java/com/example/dzairgo/fragments/ActualityFragment.java
@@ -11,11 +11,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.example.dzairgo.R;
 import com.example.dzairgo.activities.MainActivity;
 import com.example.dzairgo.adapters.ArticleAdapter;
 import com.example.dzairgo.adapters.PannelAdapter;
+import com.example.dzairgo.utils.Article;
+
+import java.util.ArrayList;
 
 
 public class ActualityFragment extends Fragment {
@@ -24,23 +28,76 @@ public class ActualityFragment extends Fragment {
     ImageView banner;
     RecyclerView pannel;
     RecyclerView.LayoutManager lm2;
+    TextView a_la_une_title;
+    TextView populaire_title;
+    TextView abonne_title;
+    ArrayList<Article> articles;
+    ArticleAdapter articleAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_actuality, container, false);
+        articles = new ArrayList<>();
+        articles.addAll(((MainActivity) (view.getContext())).articles);
         lm = new LinearLayoutManager(view.getContext());
         recyclerView = (RecyclerView) view.findViewById(R.id.recycle_view);
         recyclerView.setLayoutManager(lm);
-        ArticleAdapter articleAdapter = new ArticleAdapter(((MainActivity) (view.getContext())).articles);
+        articleAdapter = new ArticleAdapter(articles);
         recyclerView.setAdapter(articleAdapter);
-        lm2 = new LinearLayoutManager(view.getContext());
-        pannel = (RecyclerView)view.findViewById(R.id.panel);
+        lm2 = new LinearLayoutManager(view.getContext(), LinearLayoutManager.HORIZONTAL, false);
+        pannel = (RecyclerView) view.findViewById(R.id.panel);
         pannel.setLayoutManager(lm2);
         PannelAdapter pannelAdapter = new PannelAdapter(((MainActivity) view.getContext()).images);
         pannel.setAdapter(pannelAdapter);
-
+        a_la_une_title = (TextView) view.findViewById(R.id.a_la_une_title);
+        populaire_title = (TextView) view.findViewById(R.id.populaire_title);
+        abonne_title = (TextView) view.findViewById(R.id.abonne_title);
+        configPopulaireBtn();
+        configAbonneBtn();
+        configAlaUneBtn();
         return view;
     }
+
+    private void configAlaUneBtn(){
+        a_la_une_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                articles.addAll(((MainActivity) (view.getContext())).articles);
+                articleAdapter.notifyDataSetChanged();
+                a_la_une_title.setTextColor(view.getResources().getColor(R.color.green, view.getContext().getTheme()));
+                abonne_title.setTextColor(view.getResources().getColor(R.color.grey6 , view.getContext().getTheme()));
+                populaire_title.setTextColor(view.getResources().getColor(R.color.grey6 , view.getContext().getTheme()));
+
+            }
+        });
+    }
+    private void configPopulaireBtn(){
+        populaire_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                articles.clear();
+                articleAdapter.notifyDataSetChanged();
+                a_la_une_title.setTextColor(view.getResources().getColor(R.color.grey6, view.getContext().getTheme()));
+                abonne_title.setTextColor(view.getResources().getColor(R.color.grey6 , view.getContext().getTheme()));
+                populaire_title.setTextColor(view.getResources().getColor(R.color.green , view.getContext().getTheme()));
+            }
+        });
+    }
+    private void configAbonneBtn(){
+        abonne_title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                articles.clear();
+                articleAdapter.notifyDataSetChanged();
+                a_la_une_title.setTextColor(view.getResources().getColor(R.color.grey6, view.getContext().getTheme()));
+                abonne_title.setTextColor(view.getResources().getColor(R.color.green , view.getContext().getTheme()));
+                populaire_title.setTextColor(view.getResources().getColor(R.color.grey6 , view.getContext().getTheme()));
+            }
+        });
+    }
+
+//    private void scrollConfig(){
+//    }
 
 }
